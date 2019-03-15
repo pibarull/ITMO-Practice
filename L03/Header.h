ea47cfb9a87f425daa7c65eb92e6a60062a784bd
Ilya <31762469+pibarull@users.noreply.github.com>
2019-03-15 18:40:39 +0300

    Add files via upload

---
+++ L03/Header.h
@@ -0,0 +1,17 @@
+//
+//  Header.h
+//  lab3
+//
+//  Created by Илья Ершов on 15/03/2019.
+//  Copyright © 2019 Ilia Ershov. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+int GCD(int a, int b);
+bool Check(int x);
+int GetLevel(int x);
+
+
+#endif /* Header_h */
